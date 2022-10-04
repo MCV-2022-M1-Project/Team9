fb45f1b1f878efae9e7158406840f4e7cad8b5dc
jblancoarnaus <j.blanco.arnaus@gmail.com>
2022-10-04 20:09:05 +0200

    added basic distance measurements

--- Museum.py
+++ Museum.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import sys
+import numpy as np
 import os
 import cv2
 from Image import Image
@@ -10,11 +11,12 @@ class Museum:
         self.dataset_directory = dataset_directory
         self.query_set_directory = query_set_directory
         self.relationships = self.read_relationships()
-        self.dataset = self.read_images(self.dataset_directory)
-        self.query_set = self.read_images(self.query_set_directory)
+        #self.dataset = self.read_images(self.dataset_directory)
+        #self.query_set = self.read_images(self.query_set_directory)
         
     def read_relationships(self):
         print(self.dataset_directory)
+        return
         relationships_path = self.dataset_directory + '/relationships.pkl'
         return pd.read_pickle(relationships_path)
     
@@ -28,25 +30,61 @@ class Museum:
         return images
     
     #Task 2
- 
-    def compute_euclidean_distance(self, image1 : Image, image2: Image) -> float:
-        pass
+    def compute_distances(self,image1:Image, image2:Image, distance_string:str) -> float:
+        """
+        Given two images and the type of distance, it computes the distance between their histograms
+            image1,image2: Image objects to be measured
+            distance_string: Contains the label of the distance that will be used to compute it
+        """
+
+        #uncomment once proper histograms are in the images
+        #histogram1 = image1.histogram_grey_scale_image
+        #histogram2 = image2.histogram_grey_scale_image
+       
+        ###temporary values for testing purposes
+        img1 = cv2.imread("bbdd_00000.jpg",0)
+        img2 = cv2.imread("bbdd_00004.jpg",0)
+        histogram1 = np.bincount(img2.ravel(), minlength = 256)
+        histogram2 = np.bincount(img1.ravel(), minlength = 256)
+        
+        if distance_string == "L2":
+            distance = self.compute_euclidean_distance(histogram1, histogram2)
+        elif distance_string =="L1":
+            distance = self.compute_L1_distance(histogram1, histogram2)
+        elif distance_string == "X2":
+            distance = self.compute_x2_distance(histogram1, histogram2)
+        elif distance_string == "HIST_INTERSECTION":
+            distance = self.compute_histogram_intersection(histogram1, histogram2)
+        elif distance_string == "HELLINGER_KERNEL":
+            distance = self.compute_hellinger_kernel(histogram1, histogram2)
+
+        print(distance)
+        return distance
+    def compute_euclidean_distance(self, histogram1 : np.ndarray, histogram2 : np.ndarray) -> float:
+        sum_sq = np.sum(np.square(histogram2 - histogram1))
+        euclidean_distance = np.sqrt(sum_sq)
+        return euclidean_distance
     
-    def compute_L1_distance(self, image1 : Image, image2: Image) -> int:
-        pass
+    def compute_L1_distance(self, histogram1 : np.ndarray, histogram2 : np.ndarray) -> float:
+        l1_distance = sum(abs(histogram2-histogram1))
+        return l1_distance
     
-    def compute_x2_distance(self, image1 : Image, image2: Image) -> float:
-        pass
+    def compute_x2_distance(self, histogram1 : np.ndarray, histogram2 : np.ndarray) -> float:
+        x2_distance = np.sum(np.square(histogram2 - histogram1)/(histogram1+histogram2))
+       
+        return x2_distance
     
-    def compute_histogram_intersection(self, image1 : Image, image2: Image) -> int:
-        pass
+    def compute_histogram_intersection(self, histogram1 : np.ndarray, histogram2 : np.ndarray) -> float:
+        histogram_intersection = np.sum(np.minimum(histogram1, histogram2))
+        return histogram_intersection
     
-    def compute_hellinger_kernel(self, image1 : Image, image2: Image) -> float:
-        pass
+    def compute_hellinger_kernel(self, histogram1 : np.ndarray, histogram2 : np.ndarray) -> float:
+        hellinger_kernel =  np.sum(np.sqrt(np.multiply(histogram1,histogram2)))
+        return hellinger_kernel
 
     
     #Task 3/4 -> For an image of the query set, retrieve the top K images with the lowest distance or the highest score from the dataset
-    def retrieve_top_K_results(self, image: Image) -> list:
+    def retrieve_top_K_results(self, image: Image, K: int) -> list:
         pass
     
     #Task 4 -> 
@@ -58,8 +96,8 @@ def main():
     dataset_directory = sys.argv[1]
     query_set_directory = sys.argv[2]
     museum = Museum(dataset_directory, query_set_directory)
-
-    
+    museum.compute_distances(None,None)
 
 if __name__ == "__main__":
     main()
+
