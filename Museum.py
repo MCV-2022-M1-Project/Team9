8814fee14045b76f5b66d6bb5ee5787eb498d160
jblancoarnaus <j.blanco.arnaus@gmail.com>
2022-10-05 19:43:41 +0200

    added offline db generation

--- Museum.py
+++ Museum.py
@@ -1,7 +1,7 @@
 """
-Score painting retrieval tasks
+Generate similarity results given a query folder
 Usage:
-  Museum.py <inputDir> <queryDir> [--distance=<dist>] [--K=<k>]
+  Museum.py <inputDir> <queryDir> [--distance=<dist>] [--K=<k>] [--picklePath=<ppath>] [--generateDB=<gendb>]
   Museum.py -h | --help
   -
   <inputDir>               Directory with database data 
@@ -9,6 +9,8 @@ Usage:
 Options:
   --distance=<dist>        Distance to compute image similarity (L1, L2, X2, HIST_INTERSECTION, HELLINGER_KERNEL) [default: L1]
   --K=<k>                  Number of similar results to output [default: 3]
+  --picklePath=<ppath>     Filename/path to save the pkl results file [default: result.pkl]
+  --generateDB=<gendb>     Regenerate database (True/False) [default: True]
 """
 
 import pandas as pd 
@@ -16,17 +18,26 @@ import sys
 import numpy as np
 import os
 import cv2
+import pickle
 from Image import Image
 from docopt import docopt
 
 
 class Museum:
-    def __init__(self, dataset_directory: str, query_set_directory: str) -> None:
+    def __init__(self, dataset_directory: str, query_set_directory: str, generateDB:bool) -> None:
         self.dataset_directory = dataset_directory
         self.query_set_directory = query_set_directory
         self.relationships = self.read_relationships()
         self.query_gt = self.read_query_gt()
-        self.dataset = self.read_images(self.dataset_directory)
+        if generateDB == "True":
+            print("Generating database pkl file")
+            self.dataset = self.read_images(self.dataset_directory)
+        else:
+            with open("database.pkl", 'rb') as f:
+                self.dataset = pickle.load(f)
+            f.close()
+
+        print("Computing query image descriptors")
         self.query_set = self.read_images(self.query_set_directory)
         
     def read_relationships(self):
@@ -49,24 +60,15 @@ class Museum:
                 
         return images
     
-    #Task 2
     def compute_distances(self,image1:Image, image2:Image, distance_string:str) -> float:
         """
-        Given two images and the type of distance, it computes the distance between their histograms
+        Given two images and the type of distance, it computes the distance/similarity between their histograms
             image1,image2: Image objects to analyse their similarities
             distance_string: Contains the label of the distance that will be used to compute it
         """
 
-        #uncomment once proper histograms are in the image
         histogram1 = image1.histogram_grey_scale_image
         histogram2 = image2.histogram_grey_scale_image
-
-       
-        ###temporary values for testing purposes
-        #img1 = cv2.imread("bbdd_00000.jpg",0)
-        #img2 = cv2.imread("bbdd_00004.jpg",0)
-        #histogram1 = np.bincount(img2.ravel(), minlength = 256)
-        #histogram2 = np.bincount(img1.ravel(), minlength = 256)
         
         #cast to int64 just in case
         histogram1 = np.int64(histogram1)
@@ -118,10 +120,18 @@ class Museum:
         return x2_distance
     
     def compute_histogram_intersection(self, histogram1 : np.ndarray, histogram2 : np.ndarray) -> float:
+        """Given two histograms, computes the histogram intersection
+            histogram1, histogram2: histograms of the images to compare the similarities of
+        Output: histogram intersection
+            """
         histogram_intersection = np.sum(np.minimum(histogram1, histogram2))
         return histogram_intersection
     
     def compute_hellinger_kernel(self, histogram1 : np.ndarray, histogram2 : np.ndarray) -> float:
+        """Given two histograms, computes hellinger kernel distance
+            histogram1, histogram2: histograms of the images to compare the similarities of
+        Output: hellinger kernel distance
+        """
         hellinger_kernel =  np.sum(np.sqrt(np.multiply(histogram1,histogram2)))
         return hellinger_kernel
 
@@ -133,25 +143,9 @@ class Museum:
                 distance_string: specifies which distance to use
             Output: list with the ids of the K most similar images to query_image
         """
-    
-        
-        ###temporary values for testing purposes REMOVE AFTER THE SYSTEM GETS JOINED
-        #img1 = Image("bbdd_00000.jpg",0)
-        #img2 = Image("bbdd_00002.jpg",1)
-        #img3 = Image("bbdd_00003.jpg",2)
-        #img4 = Image("bbdd_00004.jpg",3)
-
-        #img2.histogram_grey_scale_image =  np.bincount((cv2.cvtColor(img2.RGB_image, cv2.COLOR_BGR2GRAY)).ravel(), minlength = 256)
-        #img3.histogram_grey_scale_image =  np.bincount((cv2.cvtColor(img3.RGB_image, cv2.COLOR_BGR2GRAY)).ravel(), minlength = 256)
-        #img4.histogram_grey_scale_image = np.bincount((cv2.cvtColor(img4.RGB_image, cv2.COLOR_BGR2GRAY)).ravel(), minlength = 256)
-        #bbdd_images = [img1, img2, img3, img4]
-        #query_image = img1
-        ####
-
 
         distances = []
         ids = []
-
         for BBDD_current_image in self.dataset:
         #for BBDD_current_image in bbdd_images:        #remove and comment once the system gets joined (proper line is the first one)
             current_distance = self.compute_distances(BBDD_current_image, query_image,distance_string )
@@ -166,7 +160,7 @@ class Museum:
         if(distance_string=="HIST_INTERSECTION" or distance_string =="HELLINGER_KERNEL"):
             list_distance_ids.sort(reverse = True)
         else:
-        #sort descending to ascending if the smallest score means the bigger the similarity
+        #sort descending to ascending if the smallest distance  means the bigger the similarity
             list_distance_ids.sort()
         ids_sorted = [distances for ids, distances in list_distance_ids]
         return ids_sorted[:K]
@@ -208,7 +202,7 @@ class Museum:
         return score / min(len(actual), k)
     def compute_MAP_at_k(self, actual, predicted, k):
         """
-        Code obtained from the benhammer repo https://github.com/benhamner/Metrics/blob/master/Python/ml_metrics/average_precision.py
+        Code belongs to the benhammer repo https://github.com/benhamner/Metrics/blob/master/Python/ml_metrics/average_precision.py
         Computes the mean average precision at k.
         This function computes the mean average prescision at k between two lists
         of lists of items.
@@ -227,7 +221,6 @@ class Museum:
         return np.mean([self.apk(a,p,k) for a,p in zip(actual, predicted)])
     
     
-
 def main():
     # read arguments
     args = docopt(__doc__)
@@ -235,22 +228,39 @@ def main():
     query_set_directory = args['<queryDir>']
     distance_arg = args['--distance']
     K = int(args['--K'])
-    
+    pickle_path = args['--picklePath']
+    generateDB = args['--generateDB']
+    museum = Museum(dataset_directory, query_set_directory,generateDB)
+    if generateDB:
+        image_list = []
+        ##STORE DATABASE INTO FILE
+        for BBDD_current_image in museum.dataset:
+            image_list.append(BBDD_current_image)
+        
+        #save list of lists into pkl file
+        with open("database.pkl", 'wb') as f:
+            pickle.dump(image_list, f)
+            
 
-    museum = Museum(dataset_directory, query_set_directory)
+    ##GENERATE QUERY RESULTS
     predicted_top_K_results = []    #list containing in each position a K-element list of the predictions for that query
     #for each one of the queries
+    
+    print("Computing distances with DB images...")
     for current_query in museum.query_set:
         predicted_top_K_results.append(museum.retrieve_top_K_results(current_query,K,distance_arg))
     
-    print("querygt",museum.query_gt)
-    print("predictions",predicted_top_K_results)
+    #print("querygt",museum.query_gt)
+    #print("predictions",predicted_top_K_results)
     mapk_score = museum.compute_MAP_at_k(museum.query_gt, predicted_top_K_results, K)
     
     print("Using distance", distance_arg)
-    print("TOP ",K, " RESULTS: ",predicted_top_K_results)
-    print(mapk_score)
+    #print("TOP ",K, " RESULTS: ",predicted_top_K_results)
+    print("MAPK score: ",mapk_score)
 
+    #save list of lists into pkl file
+    with open(pickle_path, 'wb') as f:
+        pickle.dump(predicted_top_K_results, f)
 if __name__ == "__main__":
     main()
 
